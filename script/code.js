2d065ee6e47e2c21c8522d81bdd698ad5b17e111
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-16 08:46:57 +0200

    rubricAdded

--- script/code.js
+++ script/code.js
@@ -1,16 +1,20 @@
 let numbers = [12, 18, 11, 5, 8, 2];
 //console.table(numbers)
-let btnSubmit = document.querySelector('[data-submit]')
-function displayMidNum(N) {
-    let output = document.querySelector('[data-output]')
-    switch (N){
-        case numbers:
-        output.innerText = displayMidNum(N[2]);
-        output.innerText = displayMidNum(N[3]);
-        break;
-        default:
-        output.innerText = `My code is broken`;
-        break;
-    }
-    btnSubmit.addEventListener('click',displayMidNum)
+// let btnSubmit = document.querySelector('[data-submit]')
+// function displayMidNum(N) {
+//     let output = document.querySelector('[data-output]')
+//     switch (N){
+//         case numbers:
+//         output.innerText = displayMidNum(N[2]);
+//         output.innerText = displayMidNum(N[3]);
+//         break;
+//         default:
+//         output.innerText = `My code is broken`;
+//         break;
+//     }
+//     btnSubmit.addEventListener('click',displayMidNum)
+// }
+let middleIndex = parseInt(numbers.length/2) 
+if (middleIndex % 2 == 0) {
+    numbers[middleIndex]
 }
